6c6caa3859de6e4aa1d13a416da9eda9f639c37c
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 14:30:54 +0000

    Seeds file edited for generated data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |magic|
+  Magic.create!(
+    title:"#{magic} Bananas",
+    body:"MAny bannansa and more abandasns."
+  )
+end
+
+puts "10 portfolios created."
+
+5.times do |lassy|
+  Skill.create!(title:"#{lassy}",
+  percent_utilized:"10")
+end
+
+puts "5 portfolios created."
+
+8.times do |lassy|
+  Portfolio.create!(
+    subtitle:"#{lassy}",
+    body:"Kopplye janes and marty.",
+    main_image:"http://placehold.it/800x600",
+    thumb_image:"http://placehold.it/200x100"
+    )
+end
+
+puts "8 portfolios created."
