4a5d77a9c1b545044f97056a76cb1c2b04b64a02
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 15:48:17 +0000

    Portfolio Creat and New

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,20 @@ class PortfoliosController < ApplicationController
     @port = Portfolio.all
   end
   
+  def new
+    @port = Portfolio.new
+  end
+  
+  def create
+    @port = Portfolio.new(params.require(:portfolio).permit(:subtitle, :body))
+
+    respond_to do |format|
+      if @port.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
