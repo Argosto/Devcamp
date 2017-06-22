7aa475e45f71c5e31a7f429bdaebc2ee0504253a
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:20:02 +0000

    Default Vaulues

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
   
   scope :ruby_port_string, -> {where(subtitle:'Ruby on Rails')}
+  
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||='http://placehold.it/800x600'
+    self.thumb_image ||='http://placehold.it/200x100'
+  end
 end
