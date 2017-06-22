ef7e69cdb61c273ecda5431c098d26d4c7765cce
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:48:10 +0000

    Added Concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+  
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
   
   def self.angular
@@ -10,7 +12,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||='http://placehold.it/800x600'
-    self.thumb_image ||='http://placehold.it/200x100'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '200', width: '100')
   end
 end
